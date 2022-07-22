2e048d70f828cd7bb464f770ca01b161cd0bc7a2
rober1992 <82915340+rober1992@users.noreply.github.com>
2022-07-22 14:20:08 -0300

    Add files via upload

---
+++ pages/sobre-mi.php
@@ -0,0 +1,59 @@
+<?php $page = 'sobre-mi';include '../template/navbar.php';?>
+
+<div class="sobre-mi">
+    <div class="divSobremi">
+        <p class="sobre-mi_texto">Soy una cabeza pixelada</p>
+    </div>
+    <img class="sobre-mi_img" src="../img/roberto.png" alt="">
+
+</div>
+
+<article class="quien-soy-art">
+    <p class="quien-soy-art_p1" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">Hola! mi nombre es Roberto, te preguntarás que es lo que hago seguramente, básicamente puedo conjurar un sitio web de la nada
+         a la existencia, completamente formado. No uso trucos, ni esto es magia. Me especializo en manipular la estructura misma de la web, y sus materias primas de 
+         HTML, CSS, y JavaScript / PHP.</p>
+    <p class="quien-soy-art_p2" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">Para esto vengo estudiando hace algunos años las extrañas artes de la programación, siendo un asiduo seguidor del desarrollo Front-end.</p>
+    <p class="quien-soy-art_p3" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">En cuanto a mi, tengo 29 años, 2 hijos que en ocasiones se portan bien, me gusta jugar a videojuegos viejos (de ahí mi idea de hacer un diseño de pixeles) igual tampoco tan viejos, solo quería exagerar un poco!</p>
+</article>
+
+
+
+<main class="main-sobremi" data-aos="zoom-in-up"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="1200">
+		<h1 class="titulo">Preguntas Frecuentes</h1>
+
+		<div class="preguntas">
+
+			<div class="contenedor-preguntas activo" data-categoria="metodos-pago">
+				<div class="contenedor-pregunta">
+					<p class="pregunta">¿Trabajas en proyectos grandes tambien?<img src="../img/suma.svg" alt="Abrir Respuesta" /></p>
+					<p class="respuesta">Por ahora no tuve oportunidad de trabajar en uno con esas caracteristicas, quizas si volvés aca en un tiempo veas una respuesta distinta <img class="emoji" src="../img/guiño.png" alt=""></p>
+				</div>
+				<div class="contenedor-pregunta">
+					<p class="pregunta">¿Hacia donde queres apuntar en un futuro?<img src="../img/suma.svg" alt="Abrir Respuesta" /></p>
+					<p class="respuesta">En este momento quiero dedicarme al desarrollo Front-end que es lo que mas me gusta, pero no se, quizas en un futuro podria llegar a ser Full-Stack, quien sabe <img class="emoji" src="../img/nerd.png" alt=""></p>
+				</div>
+        		<div class="contenedor-pregunta">
+					<p class="pregunta">¿Estas al tanto de las últimas tendencias en diseño?<img src="../img/suma.svg" alt="Abrir Respuesta" /></p>
+					<p class="respuesta">A veces veo programas de remodelacion de casas, asi que si, se de que color quedan mejor las alacenas de una cocina, pará, no se si entendi la pregunta <img class="emoji" src="../img/tonto.png" alt=""></p>
+				</div>
+				<div class="contenedor-pregunta">
+					<p class="pregunta">¿Por que no ponés también tu instagram?<img src="../img/suma.svg" alt="Abrir Respuesta" /></p>
+					<p class="respuesta">Lo interesante esta acá, no hay nada divertido que ver ahi <img class="emoji" src="../img/aburrido.png" alt=""></p>
+				</div>
+				<div class="contenedor-pregunta">
+					<p class="pregunta">¿Siempre fuiste programador?<img src="../img/suma.svg" alt="Abrir Respuesta" /></p>
+					<p class="respuesta">No, trabajé muchos años de mi vida de en la cocina, soy Cheff, esto no es un chiste <img class="emoji" src="../img/sorprendido.png" alt=""></p>
+				</div>
+        		<div class="contenedor-pregunta genial">
+					<p class="pregunta">¿Como hago para ser tan genial como vos?<img src="../img/suma.svg" alt="Abrir Respuesta" /></p>
+					<p class="respuesta genial">Eso es <br> imposible</p>
+				</div>
+			</div>
+
+		</div>
+	</main>
+
+
+<?php include '../template/footer.php';?>
