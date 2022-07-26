d04ce0431b58b4e17ea5fbc275eedb270bbbf36e
rober1992 <82915340+rober1992@users.noreply.github.com>
2022-07-26 17:01:34 -0300

    Add files via upload

---
+++ server/sendMail.js
@@ -0,0 +1,27 @@
+const btn = document.getElementById('botonEnviar');
+
+document.getElementById('waterform')
+.addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_zzjdqcp';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+        botonEnviar.value = 'Enviar';
+        Swal.fire({
+            title: 'Mensaje enviado',
+            text: "En breve me contactaré con vos!",
+            icon: 'success',
+            confirmButtonColor: 'yellow',
+            confirmButtonText: '<a id="botonEnviado" href="../index.html">Volver al inicio</a>'
+          });
+    }, (err) => {
+        botonEnviar.value = 'Enviar';
+      alert(JSON.stringify(err));
+    });
+});
+
